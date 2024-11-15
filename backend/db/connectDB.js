ed249f33a4ec2110ba54f2f1162bfeff091798f9
aman-0341 <amanbudhraja18@gmail.com>
2024-11-15 16:39:01 +0530

    Update connectDB.js

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect('mongodb+srv://aman100:aman104@nodetuts.sfzoomu.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts');
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (err) {
 		console.error(`Error: ${err.message}`);
